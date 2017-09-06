85f19c4c171d7353dc270e4014325648aaabaf67
apple <153614131@qq.com>
2017-09-07 01:14:33 +0800

    update

--- app/src/main/kotlin/com/fashare/mvvm_juejin/viewmodel/TabVM.kt
+++ app/src/main/kotlin/com/fashare/mvvm_juejin/viewmodel/TabVM.kt
@@ -1,11 +1,8 @@
 package com.fashare.mvvm_juejin.viewmodel
 
-import android.databinding.ObservableArrayList
-import android.databinding.ObservableList
-import android.support.annotation.DrawableRes
+import com.fashare.mvvm_juejin.BR
 import com.fashare.mvvm_juejin.R
 import me.tatarka.bindingcollectionadapter.ItemView
-import java.util.*
 
 /**
  * <pre>
@@ -15,17 +12,7 @@ import java.util.*
  * </pre>
  */
 class TabVM{
-    var img = "https://img.mp.itc.cn/upload/20160506/0afc31ee80dd444687b2b52fb21ec5e3_th.png"
-    var text = "haha"
-    var itemView: ItemView = ItemView.of(com.fashare.mvvm_juejin.BR.viewModel, R.layout.item_main_tab)
-    var itemList: ObservableList<Any> = ObservableArrayList<Any>().apply{
-        this.addAll(Arrays.asList(
-                ItemVM(R.mipmap.ic_launcher),
-                ItemVM(R.mipmap.ic_launcher),
-                ItemVM(R.mipmap.ic_launcher),
-                ItemVM(R.mipmap.ic_launcher)
-        ))
-    }
-
-    class ItemVM(@DrawableRes var iconRes: Int)
+//    var img = "https://img.mp.itc.cn/upload/20160506/0afc31ee80dd444687b2b52fb21ec5e3_th.png"
+    var itemView = ItemView.of(BR.viewModel, R.layout.item_main_tab)
+    var itemList = arrayOf(R.mipmap.ic_launcher, R.mipmap.ic_launcher)
 }
--- databinding/src/main/kotlin/com/fashare/databinding/adapters/ViewGroupAdapter.java
+++ databinding/src/main/kotlin/com/fashare/databinding/adapters/ViewGroupAdapter.java
@@ -2,17 +2,25 @@ package com.fashare.databinding.adapters;
 
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
-import android.databinding.ObservableList;
 import android.databinding.ViewDataBinding;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.Arrays;
+import java.util.List;
 
 import me.tatarka.bindingcollectionadapter.ItemView;
 
 public final class ViewGroupAdapter {
     @BindingAdapter({"itemView", "viewModels"})
-    public static void addViews(ViewGroup viewGroup, final ItemView itemView, final ObservableList<?> viewModelList) {
+    public static void addViews(ViewGroup viewGroup, final ItemView itemView, final Object[] viewModelList) {
+        if (viewModelList != null && viewModelList.length > 0) {
+            addViews(viewGroup, itemView, Arrays.asList(viewModelList));
+        }
+    }
+
+    @BindingAdapter({"itemView", "viewModels"})
+    public static void addViews(ViewGroup viewGroup, final ItemView itemView, final List<?> viewModelList) {
         if (viewModelList != null && !viewModelList.isEmpty()) {
             viewGroup.removeAllViews();
             for (Object viewModel : viewModelList) {
